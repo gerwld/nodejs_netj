3454ee6b860bae939604554364bff04957406693
P Jaworski <pjaworski.dev@gmail.com>
2025-02-04 17:11:32 +0100

    html routes

--- server.js
+++ server.js
@@ -6,8 +6,23 @@ const server = http.createServer((req, res) => {
    // 0. set headers
    res.setHeader('Content-Type', 'text/html');
 
+   // path
+   let path = './views/';
+
+   switch(req.url) {
+    case '/':
+        path += 'index.html'
+        break; 
+    case '/about':
+        path += 'about.html'
+        break;
+    default:
+        path += '404.html';
+        break;
+   }
+
    // 1. send html file
-   fs.readFile('./views/index.html', (err, data) => {
+   fs.readFile(path, (err, data) => {
       if (err) console.log(err);
       else res.write(data.toString());
       res.end();
