3ac767e22f87e6c2c433431f7fe5ab59d19f1f35
P Jaworski <pjaworski.dev@gmail.com>
2025-02-04 17:22:22 +0100

    redirect

--- server.js
+++ server.js
@@ -12,12 +12,19 @@ const server = http.createServer((req, res) => {
    switch(req.url) {
     case '/':
         path += 'index.html'
+        res.statusCode = 200;
         break; 
     case '/about':
         path += 'about.html'
+        res.statusCode = 200;
+        break;
+    case '/about-me':
+        res.setHeader('Location', '/about');
+        res.statusCode = 301;
         break;
     default:
         path += '404.html';
+        res.statusCode = 404;
         break;
    }
 
