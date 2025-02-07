6d15a762af68da66eb5778b683ee7c3002df47cc
P Jaworski <pjaworski.dev@gmail.com>
2025-02-07 14:52:12 +0100

    added ejs modules

--- app.js
+++ app.js
@@ -3,23 +3,29 @@ const express = require('express');
 // express app
 const app = express();
 
+// register new engine
+app.set('view engine', 'ejs');
+
 // listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
-   res.sendFile('views/index.html', { root: __dirname });
+   res.render('index', {title: "home", blogs: []})
 });
 app.get('/about', (req, res) => {
-   res.sendFile('views/about.html', { root: __dirname });
+   res.render('about' , {title: "",})
 });
+app.get('/blogs/create', (req, res) => {
+    res.render('blog' , {title: "",})
+ });
 
 // redirects
 app.get('/about-me', (req, res) => {
-   res.redirect('/about');
+   res.redirect('/about' , {title: "",})
 });
 
 app.get('*', (req, res) => {
-   res.status(404).sendFile('views/404.html', { root: __dirname });
+   res.status(404).render('404' , {title: "",})
 });
 
 app.use((_, res) => res.status(400).json({ message: 'Unauthorized' }));
